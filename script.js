fbad042505468922fda6d66b70ac93e886a92877
Alyssa Falcione <lyzarddz@gmail.com>
2021-11-24 02:28:31 -0500

    Services and Home page Complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
